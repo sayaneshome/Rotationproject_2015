7c239933cc9c265fb21c94bb6e6cc7d664fe5cd0
sayane shome <sayane.shome2009@vit.ac.in>
2016-02-14 03:57:29 -0600

    new output

--- test_circos.py
+++ test_circos.py
@@ -4,16 +4,15 @@ import random
 
 with open('karyotype.zeamays1.txt', 'r') as f:
     reader = csv.reader(f,delimiter='\t')
-    see=open('output1.txt','w')
+    see=open('output.txt','w')
+    colors=open("colours_bar.txt","r")
+    lines=colors.read().split() 
     for row in reader:
-        lines=[]
-        colors=open("colours_bar.txt","r")
-        lines=colors.read().split()
         color_me=random.choice(lines)
-        lines.remove(color_me)
         row.append(color_me)
-        row = [x.strip(' ') for x in row]
-        see.write("%s" % '\t'.join(row))
+        lines.remove(color_me)
+        row = [x.strip('') for x in row]
+        see.write("%s\n" %'\t'.join(row))
     see.close()
     
   
